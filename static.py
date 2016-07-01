7dd6c6a9827f2b391dba553b25c7de6d9a864f7f
Nolan Prescott <prescott.nolan@gmail.com>
2016-07-01 03:45:29 -0500

    Proof of concept, working rough draft
     - No fancy templates
     - No command line interface
     - No tests
     - No feeds

---
+++ static.py
@@ -0,0 +1,64 @@
+import json
+import sys
+import os
+import re
+from mistune import Markdown
+from jinja2 import Environment, PackageLoader
+
+# TODO: lots of functions/no-self use happening here
+class StaticGenerator():
+    def __init__(self, config_file="config.json"):
+        self.config_file = config_file
+        self.config = self.read_config_file()
+        self._separator = re.compile(r'^===$', re.MULTILINE)
+        self.markdown = Markdown()
+
+    def read_config_file(self):
+        with open(self.config_file) as f:
+            config = json.load(f)
+        return config
+
+    def read_file(self, filename):
+        with open(filename) as f:
+            contents = f.read()
+        return contents
+
+    def write_output_file(self, contents, path):
+        os.makedirs(os.path.dirname(path), exist_ok=True)
+        with open(path, mode='w', encoding='utf8') as outfile:
+            outfile.write(contents)
+
+    def templatize_post(self, contents):
+        env = Environment(loader=PackageLoader('static', 'templates'))
+        template = env.get_template('post.html')
+        return template.render(post_body=contents)
+
+    def collect_posts(self, from_dir):
+        for root, _, files in os.walk(from_dir):
+            for _file in files:
+                if _file.endswith('.md'):
+                    yield (root, _file)
+
+    def parse_post(self, content):
+        _split_contents = self._separator.split(content, maxsplit=1)
+        if len(_split_contents) < 2:
+            raise SyntaxError("Failed to parse Post header")
+        header_string, post_body = _split_contents
+        kv_string = header_string.strip().split('\n')
+        header = {k: v.strip() for k,v in (i.split(':', maxsplit=1)
+                                           for i in kv_string)}
+        return header, post_body
+
+    def create_posts(self):
+        posts_dir = self.config['posts_dir']
+        output_dir = self.config['output_dir']
+
+        for directory, filename in self.collect_posts(posts_dir):
+            post_file = os.path.join(directory, filename)
+            raw_text = self.read_file(post_file)
+            header, post_body = self.parse_post(raw_text)
+            markup = self.markdown(post_body)
+            templated_html = self.templatize_post(markup)
+            _outdir = re.sub(posts_dir, output_dir, directory)
+            out_path = os.path.join(_outdir, filename)
+            self.write_output_file(templated_html, out_path)
---
+++ temp/posts/2016/test.md
@@ -0,0 +1,11 @@
+Title: This a Title
+Date: 2016-06-30
+===
+
+# Title line, wow!
+===
+this is a paragraph
+ - bullet
+ - points
+ - yes
+===
