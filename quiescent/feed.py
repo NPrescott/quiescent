379e98863e381f19d753230a3d5292c507d8601d
Nolan Prescott <prescott.nolan@gmail.com>
2018-01-21 15:55:21 -0500

    Refactor internals
     - cleaner separation of concerns
     - less duplication
     - add tests for the Atom feed
     - fewer tests in general :(
    
    In general, the "imperative shell" that is the StaticGenerator class is
    still difficult to test (and has none). Not sure how best to fix this,
    short of writing integration tests with full file reads and writes.

--- quiescent/feed.py
+++ quiescent/feed.py
@@ -13,53 +13,39 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-"""Atom feed generator
-----------------------
-
-Revamping the (broken) feed generation capabilities in Static, I'm opting for
-Atom because it seemed simpler, but I haven't read either spec entirely[0]. I'm
-finally dropping all attempts at string-templating because, well, string
-templates and XML :(
-
-First pass here does the simplest thing and is relatively feature-less.
+"""
+Atom feed[0] generator
   - update times are reported in UTC with no offset
 
-----------------------------------------
 [0]: https://tools.ietf.org/html/rfc4287
 """
 import xml.etree.ElementTree as ET
-from datetime import datetime, timezone
 from urllib.parse import urljoin
-import logging
 
 
-logger = logging.getLogger(__name__)
 ET.register_namespace('', 'http://www.w3.org/2005/Atom')
 
-def feed(all_posts, **config):
-    try:
-        feed = ET.Element('{http://www.w3.org/2005/Atom}feed')
-        title = ET.SubElement(feed, 'title')
-        title.text = config['name']
-        link = ET.SubElement(feed, 'link')
-        link.attrib['href'] = config['domain']
-        feed_link = ET.SubElement(feed, 'link')
-        feed_link.attrib['href'] = urljoin(config['domain'], config['feed link'])
-        feed_link.attrib['rel'] = 'self'
-        updated = ET.SubElement(feed, 'updated')
-        updated.text = datetime.now(timezone.utc).isoformat()
-        author = ET.SubElement(feed, 'author')
-        name = ET.SubElement(author, 'name')
-        name.text = config['author']
-        feed_id = ET.SubElement(feed, 'id')
-        feed_id.text = config['domain']
-        for post in all_posts:
-            feed_entry(feed, post, domain=config['domain'])
-        return feed
-    except KeyError as e:
-        raise KeyError(f"Configuration missing entry for: {e}")
+def _feed(all_posts, date=None, name=None, domain=None, feed_link=None, feed_author=None):
+    _feed = ET.Element('{http://www.w3.org/2005/Atom}feed')
+    title = ET.SubElement(_feed, 'title')
+    title.text = name
+    link = ET.SubElement(_feed, 'link')
+    link.attrib['href'] = domain
+    feed_link = ET.SubElement(_feed, 'link')
+    feed_link.attrib['href'] = urljoin(domain, feed_link)
+    feed_link.attrib['rel'] = 'self'
+    updated = ET.SubElement(_feed, 'updated')
+    updated.text = date.isoformat()
+    author = ET.SubElement(_feed, 'author')
+    name = ET.SubElement(author, 'name')
+    name.text = feed_author
+    feed_id = ET.SubElement(_feed, 'id')
+    feed_id.text = domain
+    for post in all_posts:
+        _feed_entry(_feed, post, domain=domain)
+    return _feed
 
-def feed_entry(parent_element, post, domain=None):
+def _feed_entry(parent_element, post, domain=None):
     entry = ET.SubElement(parent_element, 'entry')
     title = ET.SubElement(entry, 'title')
     title.text = post.title
@@ -71,8 +57,14 @@ def feed_entry(parent_element, post, domain=None):
     updated.text = post.date.isoformat()
     content = ET.SubElement(entry, 'content')
     content.attrib['type'] = 'html'
-    content.text = post.body_markup
+    content.text = post.body
     return entry
 
-def render_feed(feed_element):
-    return ET.tostring(feed_element, encoding='utf-8')
+def feed(all_posts, date=None, name=None, domain=None, feed_link=None, feed_author=None):
+    element = _feed(all_posts,
+                    date=date,
+                    name=name,
+                    domain=domain,
+                    feed_link=feed_link,
+                    feed_author=feed_author)
+    return ET.tostring(element, encoding='unicode')
